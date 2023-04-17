106a4cc11f7ab341f0574776cae9a88d828f5283
Miika Husso <Miika.Husso@edu.savonia.fi>
2023-04-17 15:47:44 +0300

    Oikean kuvan tarkistus toimii + counter oikeille vastauksille + timeri

--- app/src/main/java/com/example/lippupeliv2/MainActivity.kt
+++ app/src/main/java/com/example/lippupeliv2/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.lippupeliv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 
@@ -15,53 +19,36 @@ class MainActivity : AppCompatActivity() {
     private lateinit var answerCard3: CardView
     private lateinit var answerCard4: CardView
     private lateinit var allFlagImageIds: MutableList<Int>
+    private lateinit var correctAnswersTextView: TextView
     private var topFlagImageId: Int = -1
+    private var correctAnswers: Int = 0
+    private lateinit var timerTextView: TextView
+    private lateinit var countDownTimer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cardslayout)
 
-        val placeholderImage1 = findViewById<ImageView>(R.id.placeholder1)
-        val placeholderImage2 = findViewById<ImageView>(R.id.placeholder2)
-        val placeholderImage3 = findViewById<ImageView>(R.id.placeholder3)
-        val placeholderImage4 = findViewById<ImageView>(R.id.placeholder4)
+        correctAnswersTextView = findViewById(R.id.correct_answers_text_view)
         flagImageView = findViewById(R.id.flag_image_view)
         answerCard1 = findViewById(R.id.answer_card_1)
         answerCard2 = findViewById(R.id.answer_card_2)
         answerCard3 = findViewById(R.id.answer_card_3)
         answerCard4 = findViewById(R.id.answer_card_4)
-        placeholderImage1.setOnClickListener {
-            // Hide placeholder image and show flags
-            placeholderImage1.visibility = View.GONE
-            answerCard1.visibility = View.VISIBLE
-            answerCard2.visibility = View.VISIBLE
-            answerCard3.visibility = View.VISIBLE
-            answerCard4.visibility = View.VISIBLE
+
+        timerTextView = findViewById(R.id.timer_text_view)
+
+        countDownTimer = object : CountDownTimer(60000, 1000) { // 1 minute duration, 1 second interval
+            override fun onTick(millisUntilFinished: Long) {
+                val remainingSeconds = millisUntilFinished / 1000
+                timerTextView.text = "Time left: $remainingSeconds seconds"
+            }
+
+            override fun onFinish() {
+                timerTextView.text = "Time's up!"
+            }
         }
-        placeholderImage2.setOnClickListener {
-            // Hide placeholder image and show flags
-            placeholderImage2.visibility = View.GONE
-            answerCard1.visibility = View.VISIBLE
-            answerCard2.visibility = View.VISIBLE
-            answerCard3.visibility = View.VISIBLE
-            answerCard4.visibility = View.VISIBLE
-        }
-        placeholderImage3.setOnClickListener {
-            // Hide placeholder image and show flags
-            placeholderImage3.visibility = View.GONE
-            answerCard1.visibility = View.VISIBLE
-            answerCard2.visibility = View.VISIBLE
-            answerCard3.visibility = View.VISIBLE
-            answerCard4.visibility = View.VISIBLE
-        }
-        placeholderImage4.setOnClickListener {
-            // Hide placeholder image and show flags
-            placeholderImage4.visibility = View.GONE
-            answerCard1.visibility = View.VISIBLE
-            answerCard2.visibility = View.VISIBLE
-            answerCard3.visibility = View.VISIBLE
-            answerCard4.visibility = View.VISIBLE
-        }
+        countDownTimer.start()
 
         allFlagImageIds = mutableListOf<Int>().apply {
             addAll(
@@ -115,37 +102,55 @@ class MainActivity : AppCompatActivity() {
 
         // Set the answer options to random flags from the shuffled list
         answerCard1.setBackgroundResource(allFlagImageIds[0])
+        answerCard1.tag = allFlagImageIds[0]
         answerCard2.setBackgroundResource(allFlagImageIds[1])
+        answerCard2.tag = allFlagImageIds[1]
         answerCard3.setBackgroundResource(allFlagImageIds[2])
+        answerCard3.tag = allFlagImageIds[2]
         answerCard4.setBackgroundResource(allFlagImageIds[3])
+        answerCard4.tag = allFlagImageIds[3]
 
         // Set one of the answer options to match the top flag image
         val correctAnswerCard = listOf(answerCard1, answerCard2, answerCard3, answerCard4).random()
         correctAnswerCard.setBackgroundResource(topFlagImageId)
+        correctAnswerCard.tag = topFlagImageId
+
         val placeholderImage1 = findViewById<ImageView>(R.id.placeholder1)
         val placeholderImage2 = findViewById<ImageView>(R.id.placeholder2)
         val placeholderImage3 = findViewById<ImageView>(R.id.placeholder3)
         val placeholderImage4 = findViewById<ImageView>(R.id.placeholder4)
-            // Hide placeholder image and show flags
-            placeholderImage1.visibility = View.VISIBLE
-            placeholderImage2.visibility = View.VISIBLE
+        placeholderImage1.visibility = View.VISIBLE
+        placeholderImage2.visibility = View.VISIBLE
         placeholderImage3.visibility = View.VISIBLE
         placeholderImage4.visibility = View.VISIBLE
     }
 
     private fun handleAnswerClick(selectedCard: CardView) {
         // Set the background image of each option to the random flag image that was previously set
-        answerCard1.setBackgroundResource(allFlagImageIds[0])
+        /*answerCard1.setBackgroundResource(allFlagImageIds[0])
         answerCard2.setBackgroundResource(allFlagImageIds[1])
         answerCard3.setBackgroundResource(allFlagImageIds[2])
-        answerCard4.setBackgroundResource(allFlagImageIds[3])
+        answerCard4.setBackgroundResource(allFlagImageIds[3])*/
 
-        // Check if the selected option matches the top flag
+        val placeholderImage1 = findViewById<ImageView>(R.id.placeholder1)
+        val placeholderImage2 = findViewById<ImageView>(R.id.placeholder2)
+        val placeholderImage3 = findViewById<ImageView>(R.id.placeholder3)
+        val placeholderImage4 = findViewById<ImageView>(R.id.placeholder4)
+        placeholderImage1.visibility = View.GONE
+        placeholderImage2.visibility = View.GONE
+        placeholderImage3.visibility = View.GONE
+        placeholderImage4.visibility = View.GONE
+
+        // Check if the background resource of the selected option matches the top flag
         if (selectedCard.tag == topFlagImageId) {
             Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+            correctAnswers++
+            correctAnswersTextView.text = correctAnswers.toString()
         } else {
             Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
         }
-        setupNewGame();
+        Handler(Looper.getMainLooper()).postDelayed({
+            setupNewGame()
+        }, 3000)
     }
 }
